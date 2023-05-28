850319dd5f018e51ec085111735e85854ca2f545
Koray Latif <i.koray@me.com>
2023-05-28 19:59:11 -0400

    Using site object to create article objects

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
+from src.site import Site
+from src.article import Article
+
 from json import dumps
-from src.reuters_us import reuters_us
-from src.ap_us import ap_us
+from src.sites import *
 
 def main():
-    articles = dict()
+    article_dict = dict()
 
-    article_list = reuters_us()
-    article_list.append(ap_us())
-    for i, article in zip(range(0, len(article_list)+1), article_list):
-        articles[f"reuters-{i}"] = article.to_dict()
+    for site in Site.Instances:
+        site.get_htmls()
+        site.get_titles()
+        site.get_dates()
+        site.get_image_urls()
+        site.get_bodies()
+        site.create_articles()
+
+    for i, article in zip(range(0, len(Article.instances)+1), Article.instances):
+        article_name = f"{article.source}-{i}"
+        article.download_image(f"data/{article_name}.png")
+        article_dict[article_name] = article.to_dict()
 
     # Write articles dictionary to file
     with open("data/articles.json", "w") as file:
-        file.write(dumps(articles, indent=4))
+        file.write(dumps(article_dict, indent=4))
 
 if __name__ == '__main__':
     main()
---
+++ src/site.py
@@ -0,0 +1,58 @@
+from src.article import Article
+import requests
+from bs4 import BeautifulSoup
+import re
+
+class Site:
+    Instances = []
+
+    def __init__(self, address, category, source,
+                 link_func, title_func, date_func, image_url_func, body_func):
+        Site.Instances.append(self)
+
+        self.link_func = link_func
+        self.title_func = title_func
+        self.date_func = date_func
+        self.image_url_func = image_url_func
+        self.body_func = body_func
+
+        self.article_htmls = []
+        self.article_titles = []
+        self.article_dates = []
+        self.article_image_urls = []
+        self.article_bodies = []
+
+        self.category = category
+        self.source = source
+        self.site_html = BeautifulSoup(requests.get(address).text, 'html.parser')
+
+    def get_htmls(self):
+        links = self.link_func(self.site_html)
+
+        for link in links:
+            article_html = BeautifulSoup(requests.get(link).text, "html.parser")
+            self.article_htmls.append(article_html)
+
+    def get_titles(self):
+        self.article_titles = self._html_loop_wrapper(self.title_func)
+
+    def get_dates(self):
+        self.article_dates = self._html_loop_wrapper(self.date_func)
+
+    def get_image_urls(self):
+        self.article_image_urls = self._html_loop_wrapper(self.image_url_func)
+
+    def get_bodies(self):
+        self.article_bodies = self._html_loop_wrapper(self.body_func)
+
+    def create_articles(self):
+        for i in range(len(self.article_htmls)-1):
+            Article(self.source, self.category, self.article_titles[i], self.article_dates[i],
+                    self.article_image_urls[i], self.article_bodies[i])
+
+    def _html_loop_wrapper(self, func):
+        array = []
+        for html in self.article_htmls:
+            var = func(html)
+            array.append(var)
+        return array
