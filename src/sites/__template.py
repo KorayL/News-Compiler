8f8182bbba61c0d91a753b86241d2b12a109e1b1
Koray Latif <i.koray@me.com>
2023-05-31 16:55:15 -0400

    Added __template.py to aid with development

---
+++ src/sites/__template.py
@@ -0,0 +1,25 @@
+from src.site import Site
+
+def get_links(site_html):
+    links = []
+    return links
+
+def get_titles(html):
+    title = ""
+    return title
+
+def get_dates(html):
+    date = ""
+    return date
+
+def get_image_urls(html):
+    image_url = ""
+    return image_url
+
+def get_bodies(html):
+    body = ""
+    return body
+
+
+address = ""
+Site(address, "", "", get_links, get_titles, get_dates, get_image_urls, get_bodies)
