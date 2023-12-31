e106459afed8f407a40d5c2304bf20d375acdca2
Koray Latif <i.koray@me.com>
2023-12-31 12:58:03 -0500

    Changed import path to be absolute.

--- src/sites/__init__.py
+++ src/sites/__init__.py
@@ -2,8 +2,7 @@ import os
 
 # contains all modules to be imported when "from sites import *" is called
 __all__ = []
-
-for file_name in os.listdir("src\\sites"):
+for file_name in os.listdir(os.path.realpath(__file__).removesuffix("__init__.py")):
     if "__" not in file_name:
         file_name = file_name[:-3]
         __all__.append(file_name)
