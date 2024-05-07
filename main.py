ed093988e924535073e3eaafbd89761d6c24ac55
Koray Latif <i.koray@me.com>
2024-05-07 19:12:34 -0400

    Efficiency Changes

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-import tkinter.messagebox as alert
+from tkinter import messagebox
 from json import dumps
 import os
+
+from multiprocessing import Pool
 
 from src.Site import Site
 from src.sites import *
 import src.flask_app as gui
 
+
+def instantiate(class_: type) -> object:
+    """
+    Used when multiprocessing to instantiate a class.
+    :param class_: The class to instantiate.
+    :return: The instantiated class.
+    """
+    return class_()
 
 def get_sites() -> list[Site]:
     """
@@ -15,8 +25,9 @@ def get_sites() -> list[Site]:
 
     print("loading sites...")
 
-    sites: list[Site]
-    sites = [site() for site in Site.__subclasses__()]
+    # Instantiate all sites
+    with Pool() as pool:
+        sites: list[Site] = pool.map(instantiate, Site.__subclasses__())
 
     return sites
 
@@ -33,7 +44,7 @@ def main():
 
     # Write articles dictionary to file
     print("writing articles to file")
-    data_path: str = fr"{os.path.realpath(__file__).removesuffix(r"\main.py")}\src\static\articles.json"
+    data_path = os.path.join(os.path.dirname(__file__), "src/static/articles.json")
     with open(data_path, "w") as file:
         file.write(dumps(article_dict, indent=4))
 
@@ -41,7 +52,13 @@ def main():
 if __name__ == '__main__':
     print("running main")
     try:
-        main()
+        # Only download articles if user wants to
+        reload: bool = messagebox.askyesnocancel("Article Download", "Would you like to download the latest articles?")
+        if reload:
+            main()
+        if reload is None:
+            exit()
+
         gui.run()
     except Exception as e:
-        alert.showerror(title="An error has occurred!", message=str(e))
+        messagebox.showerror(title="An error has occurred!", message=str(e))
